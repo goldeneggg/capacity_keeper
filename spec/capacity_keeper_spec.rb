3c276e4e4852fdf9d7ef3b65e7096a9ff4e02a81
goldeneggg <jpshadowapps@gmail.com>
2017-08-31 02:45:20 +0900

    revert version

--- spec/capacity_keeper_spec.rb
+++ spec/capacity_keeper_spec.rb
@@ -8,7 +8,7 @@ describe CapacityKeeper do
 
   describe 'VERSION' do
     it 'should have a correct version number' do
-      expect(CapacityKeeper::VERSION).to eq('0.0.2')
+      expect(CapacityKeeper::VERSION).to eq('0.0.1')
     end
   end
 
